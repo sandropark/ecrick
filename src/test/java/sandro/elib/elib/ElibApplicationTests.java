9fd8601564a7d1de5bffc0a8149b4617a86af8e6
Sandro Park <ticoba@naver.com>
2022-10-14 14:39:08 +0900

    Initial commit

---
+++ src/test/java/sandro/elib/elib/ElibApplicationTests.java
@@ -0,0 +1,13 @@
+package sandro.elib.elib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
