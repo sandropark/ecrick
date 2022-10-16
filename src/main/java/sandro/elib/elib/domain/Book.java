09f7f5d2ed7a80eab45522a0f4824778e7abe2ee
Sandro Park <ticoba@naver.com>
2022-10-16 14:56:03 +0900

    수정 - 엔티티, DTO
    * 엔티티 PK 전략 지정
    * 책 전체 조회와 상세보기를 위해서 DTO를 분리

--- src/main/java/sandro/elib/elib/domain/Book.java
+++ src/main/java/sandro/elib/elib/domain/Book.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -17,15 +18,15 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 public class Book {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     @Column(name = "book_id")
     private Long id;
-
     private String title;
     private String author;
     private String publisher;
     private LocalDateTime publicDate;
-
+    private String imageUrl;
     @OneToMany(mappedBy = "book")
     private final List<BookLibraryServiceMap> bookLibraryServices = new ArrayList<>();
 
@@ -40,6 +41,13 @@ public class Book {
         this.author = author;
         this.publisher = publisher;
         this.publicDate = publicDate;
+    }
+
+    public Book(String title, String author, String publisher, String imageUrl) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.imageUrl = imageUrl;
     }
 
     public List<LibraryServiceDto> createLocation() {
