1776d019034808d4430993bb80137493660e78c0
Sandro Park <ticoba@naver.com>
2022-10-16 14:58:02 +0900

    Feat - 검색, 페이징, 상세보기
    * 제목 검색
    * 페이징
    * 책 상세보기

--- src/main/java/sandro/elib/elib/controller/BookController.java
+++ src/main/java/sandro/elib/elib/controller/BookController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sandro.elib.elib.domain.BookSearch;
 import sandro.elib.elib.dto.BookDto;
+import sandro.elib.elib.dto.BooksDto;
 import sandro.elib.elib.service.BookQueryService;
+import sandro.elib.elib.service.Page;
 
 import java.util.List;
 
@@ -17,10 +21,19 @@ public class BookController {
     private final BookQueryService bookQueryService;
 
     @GetMapping
-    public String list(Model model) {
-        List<BookDto> bookDtos = bookQueryService.getBookDtos();
-        model.addAttribute("books", bookDtos);
+    public String list(BookSearch bookSearch, Page page, Model model) {
+        List<BooksDto> booksDtos = bookQueryService.searchBook(bookSearch, page);
+        page.setUp();
+        model.addAttribute("books", booksDtos);
+        model.addAttribute("page", page);
         return "books/list";
+    }
+
+    @GetMapping("/{bookId}")
+    public String bookDetail(@PathVariable Long bookId, Model model) {
+        BookDto book = bookQueryService.findById(bookId);
+        model.addAttribute("book", book);
+        return "books/book-detail";
     }
 
 
