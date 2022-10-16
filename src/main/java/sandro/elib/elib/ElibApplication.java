34d09e218990e4d825b95dd812fde6739c570e62
Sandro Park <ticoba@naver.com>
2022-10-16 14:51:28 +0900

    Config - mysql 설정
    도서 정보를 클롤링해뒀던 mysql로 DB를 변경했다.
    안쓰는 클래스를 제거했다.

--- src/main/java/sandro/elib/elib/ElibApplication.java
+++ src/main/java/sandro/elib/elib/ElibApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElibApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ElibApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ElibApplication.class, args);
+    }
 
 }
