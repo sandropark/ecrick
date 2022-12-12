3303e859753f17d78edd11403645029e0816cd62
Sandro Park <ticoba@naver.com>
2022-12-12 18:50:22 +0900

    루트페이지 없애고 바로 books로 redirect하기

--- src/main/java/sandro/elib/elib/web/HomeController.java
+++ src/main/java/sandro/elib/elib/web/HomeController.java
@@ -1,4 +1,4 @@
-package sandro.elib.elib.controller;
+package sandro.elib.elib.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +10,7 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        return "index";
+        return "redirect:/books";
     }
 
 }
