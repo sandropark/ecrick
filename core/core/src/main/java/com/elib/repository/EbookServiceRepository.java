97aa2dc0005edc788ae77439486cd662d370dff8
Sandro Park <ticoba@naver.com>
2022-12-18 15:46:23 +0900

    .gitignore 업데이트
    - bin이 리모트에 올라가있어서 제거했다.

--- core/core/src/main/java/com/elib/repository/EbookServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elib.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.elib.domain.EbookService;
-
-public interface EbookServiceRepository extends JpaRepository<EbookService, Long> {
-    EbookService findByName(String name);
-}
--- web/site-service/src/main/java/com/elib/web/Search.java
+++
@@ -1,9 +0,0 @@
-package com.elib.web;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class Search {
-    private String keyword;
-}
