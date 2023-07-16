af4d158d4f925bf0c05f0f556d42dcc62fba1c8c
sandropark <ticoba@naver.com>
2023-07-16 16:09:27 +0900

    배포 테스트

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,11 +15,11 @@ if docker ps | grep -q green; then
     AFTER_PORT=8081
 fi
 
-docker-compose up -d nginx
+docker compose up -d nginx
 
 echo "### $BEFORE => $AFTER ###"
 echo "1. $AFTER container up"
-docker-compose up -d $AFTER # 컨테이너 실행 port는 green이냐 blue냐에 따라 compose 파일에 정의되어 있다.
+docker compose up -d $AFTER # 컨테이너 실행 port는 green이냐 blue냐에 따라 compose 파일에 정의되어 있다.
 
 # 헬스체크
 while true;do
@@ -37,6 +37,6 @@ echo "3. change proxy port and reload nginx"
 # nginx가 새로운 포트를 리버스 프록싱하게 수정 후 nginx 재시작
 docker exec nginx sed -i "s/$BEFORE/$AFTER/g" $DEFAULT_CONF; docker exec nginx service nginx reload
 echo "4. $BEFORE container down"
-docker-compose stop $BEFORE  # 이전 버전 컨테이너 멈추기
+docker compose stop $BEFORE  # 이전 버전 컨테이너 멈추기
 echo "5. delete unused images"
 docker image prune -af    # 사용하지 않는 이미지 삭제 (새로운 버전이 배포될 때마다 새로운 이미지를 pull하기 때문)
