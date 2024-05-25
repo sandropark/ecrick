d43efcec79dca86e63807b509139195ede0c79b3
sandropark <ticoba@naver.com>
2024-05-26 08:42:20 +0900

    [update] Connect MySQL

---
+++ boot/src/main/java/com/ecrick/EcrickApp.java
@@ -0,0 +1,24 @@
+package com.ecrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class EcrickApp {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void timeZoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Locale.setDefault(Locale.KOREA);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcrickApp.class, args);
+    }
+
+}
